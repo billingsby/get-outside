6314161e9ba5fd134bc5eff8eb62a3dfe814fa27
Billingsby <mark@markbillingsby.com>
2016-02-26 00:27:46 -0700

    Marker placement working

--- js/app.js
+++ js/app.js
@@ -3,13 +3,16 @@
 var map;
 var radius;
 var activity = "hiking";
-var latitude = 39.833;
-var longitude = -98.583﻿;
+var latitude;
+var longitude;
+var trails='';
 
 
 
 // Find initial location and load map
 function initialize(location) {
+  latitude = 39.833;
+  longitude = -98.583﻿;
   map = L.map('map').setView([latitude, longitude], 4);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -25,14 +28,16 @@ var geoCode = function(inputAddress) {
     address: inputAddress 
   };
   geocoder.geocode(geocoderRequest, function(results, status){
-    console.log(results);
     latitude = results[0].geometry.location.lat();
     longitude = results[0].geometry.location.lng();
+    console.log(latitude);
+    console.log(longitude);
     map.panTo(new L.LatLng(latitude, longitude));
     map.zoomIn(6);
-
-});
-}
+    getTrails(latitude, longitude);
+  });
+  
+};
 
 
 // Get activity selected
@@ -46,13 +51,14 @@ var geoCode = function(inputAddress) {
   };
 
 // Trails request
-var getTrails = function() {
+var getTrails = function(latitude, longitude) {
 var params = {
   'q[activities_activity_type_name_eq]': activity,
   lat: latitude,
   lon: longitude,
   radius: radius
 }
+console.log(params);
 function setHeader(xhr) {
   xhr.setRequestHeader('X-Mashape-Key', '4YanT409HXmshDOtn4QkTe1DNY33p1EVI2VjsnSlO6ccfOgjRd');
   xhr.setRequestHeader('Accept', 'text/plain');
@@ -65,20 +71,19 @@ $.ajax({
     type: "GET",
   })
 .done(function(result){ 
-    console.log(result);
-    
-    $.each(result.places, function(i, item) {
-     var lat = item.lat;
-     var lon = item.lon; 
-     console.log(lat);
-     console.log(lon);
+  console.log(result.places);
+  $.each(result.places, function(i, places) {
+    var lat = places.lat;
+    var lon = places.lon;
+   
      var marker = new L.marker([lat, lon]).addTo(map);
-     
-
-      
-    });
+  });
+    
+    
   })
 };
+
+
 
   
   // Slider Functions
@@ -102,7 +107,6 @@ $(document).ready(function() {
     e.preventDefault();
     var inputAddress = $('#address').val();
     geoCode(inputAddress);
-    getTrails();
   });
   
 
